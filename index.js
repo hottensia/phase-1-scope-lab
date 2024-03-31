d6bf3bee37f747927cdc738d7c39f2708e727089
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-03-31 07:37:50 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+var BestCustomer;
+function setBestCustomer(customerName) {
+    setCustomerName = 'not bob';
+}
+function overwriteBestCustomer(customerName) {
+    setBestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Ann';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Andrew';
+}
